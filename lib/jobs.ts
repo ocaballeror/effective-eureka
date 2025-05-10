3ca119e46e263850a95524ef81ad6164167ea28b
Oscar Caballero <ocaballeror@tutanota.com>
2025-05-10 23:59:15 +0200

    Rewrite ignored jobs into a "context manager"

--- lib/jobs.ts
+++ lib/jobs.ts
@@ -14,42 +14,41 @@ export type Job = {
 
 const DATA = path.resolve('./data/jobs.json');
 
-async function retrieveIgnored(): Promise<Job[]> {
+async function withIgnoredJobs(
+    callback: ((ignored: Job[]) => void | Promise<void>) | void
+): Promise<Job[]> {
     const store = getStore('jobs');
 
-    const ignored = await store.get('ignored-jobs', { type: 'json' });
-    if(ignored) console.log("Retrieved " + ignored.length + " ignored jobs");
+    const ignored = (await store.get('ignored-jobs', { type: 'json' })) || [];
+    if (ignored.length) console.log("Retrieved " + ignored.length + " ignored jobs");
     else console.log("New list of ignored jobs");
-    return ignored || [];
-}
 
-async function updateIgnored(ignored: Job[]): Promise<void> {
-    const store = getStore('jobs');
+    if (callback) {
+        await callback(ignored);
 
-    console.log("Updating list with " + ignored.length + " ignored jobs");
-    await store.setJSON('ignored-jobs', ignored);
+        console.log("Updating list with " + ignored.length + " ignored jobs");
+        await store.setJSON('ignored-jobs', ignored);
+    }
+
+    return ignored;
 }
 
 export async function readJobs(): Promise<Job[]> {
     const jobs: Job[] = JSON.parse(fs.readFileSync(DATA, 'utf8'));
 
-    const ignored = await retrieveIgnored();
-    return jobs.filter(item1 => !ignored.some(item2 => item2.id === item1.id));
+    const ignored = await withIgnoredJobs();
+    return jobs.filter(item1 => !ignored.some(item2 => item2.id == item1.id));
 }
 
 export async function ignoreJob(jobId: number): Promise<void> {
     console.log("Ignoring job " + jobId);
-    const ignored = await retrieveIgnored();
     const allJobs: Job[] = JSON.parse(fs.readFileSync(DATA, 'utf8'));
 
-    if (!ignored.some(item => item.id == jobId)) {
-        const job = allJobs.find(j => j.id == jobId);
-        if (job) {
-            ignored.push(job);
-            await updateIgnored(ignored);
-        } else
-            console.log("Cannot find job");
-    }
-    else 
-        console.log("Job already ignored");
+    await withIgnoredJobs(async (ignored) => {
+        if (!ignored.some(item => item.id == jobId)) {
+            const job = allJobs.find(j => j.id == jobId);
+            job ? ignored.push(job) : console.log("Cannot find job");
+        }
+        else console.log("Job already ignored");
+    });
 }
