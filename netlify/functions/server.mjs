f57ee8baf81f5d4aff1acf38c3383f7d45c51711
Oscar Caballero <ocaballeror@tutanota.com>
2025-05-10 21:00:46 +0200

    Set up netlify config

---
+++ netlify/functions/server.mjs
@@ -0,0 +1,36 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const serverless = require('serverless-http');
+const app = express();
+const router = express.Router();
+// app.use(express.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+const DATA = 'jobs.json';
+
+function readJobs() {
+  return JSON.parse(fs.readFileSync(DATA, 'utf8'));
+}
+
+function writeJobs(arr) {
+  fs.writeFileSync(DATA, JSON.stringify(arr, null,2));
+}
+
+router.get('/jobs', (req, res) => {
+  const jobs = readJobs().map((job, i) => ({ id: i, ...job }));
+  res.json(jobs);
+});
+
+router.delete('/jobs/:id', (req, res) => {
+  const id = parseInt(req.params.id,10);
+  let jobs = readJobs();
+  if (id < 0 || id >= jobs.length) return res.status(404).send();
+  jobs.splice(id,1);
+  writeJobs(jobs);
+  res.sendStatus(204);
+});
+
+// app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.use("/api", router);
+export const handler = serverless(app);
