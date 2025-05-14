bcf33c7352775392dfb50bc0d349f07f88401dc1
Oscar Caballero <oscar.caballero@bayes.gg>
2025-05-14 12:43:48 +0200

    Generic store manager

--- lib/jobs.ts
+++ lib/jobs.ts
@@ -18,66 +18,29 @@ export type Job = {
 const DATA = path.resolve('./data/jobs.json');
 
 
-async function withIgnoredJobs(
-    callback: ((ignored: Job[]) => void | Promise<void>) | void
-): Promise<Job[]> {
+async function manageList<T>(
+    key: string,
+    callback?: (items: T[]) => void | Promise<void>
+): Promise<T[]> {
     const store = getStore('jobs');
 
-    const ignored = (await store.get('ignored-jobs', { type: 'json' })) || [];
-    console.log("Retrieved " + ignored.length + " ignored jobs");
-
+    const data = ((await store.get(key, { type: 'json' })) || []) as T[];
     if (callback) {
-        await callback(ignored);
-
-        console.log("Updating list with " + ignored.length + " ignored jobs");
-        await store.setJSON('ignored-jobs', ignored);
+        await callback(data);
+        console.log(`Updating list ${key} with ${data.length} items`);
+        await store.setJSON(key, data);
     }
 
-    return ignored;
+    return data;
 }
 
-async function withViewedJobs(
-    callback: ((viewed: string[]) => void | Promise<void>) | void
-): Promise<string[]> {
-    const store = getStore('jobs');
-
-    const viewed: string[] = ((await store.get('viewed', { type: 'json' })) || []).map((item: number | string) => item.toString());
-    console.log("Retrieved " + viewed.length + " viewed jobs");
-
-    if (callback) {
-        await callback(viewed);
-
-        console.log("Updating list with " + viewed.length + " viewed jobs");
-        await store.setJSON('viewed', viewed);
-    }
-
-    return viewed;
-}
-
-async function withAppliedJobs(
-    callback: ((viewed: string[]) => void | Promise<void>) | void
-): Promise<string[]> {
-    const store = getStore('jobs');
-
-    const applied: string[] = ((await store.get('applied', { type: 'json' })) || []).map((item: number | string) => item.toString());
-    console.log("Retrieved " + applied.length + " applied jobs");
-
-    if (callback) {
-        await callback(applied);
-
-        console.log("Updating list with " + applied.length + " applied jobs");
-        await store.setJSON('applied', applied);
-    }
-
-    return applied;
-}
 
 export async function readJobs(): Promise<Job[]> {
     const baseJobs: Job[] = JSON.parse(fs.readFileSync(DATA, 'utf8'));
 
-    const ignored = await withIgnoredJobs();
-    const viewed = await withViewedJobs();
-    const applied = await withAppliedJobs();
+    const ignored = await manageList<Job>('ignored-jobs');
+    const viewed = await manageList<string>('viewed');
+    const applied = await manageList<string>('applied');
 
     const jobs = baseJobs
         .filter(item1 => !ignored.some(item2 => item2.id == item1.id))
@@ -97,7 +60,7 @@ export async function ignoreJob(jobId: string): Promise<void> {
     console.log("Ignoring job " + jobId);
     const allJobs: Job[] = JSON.parse(fs.readFileSync(DATA, 'utf8'));
 
-    await withIgnoredJobs(async (ignored) => {
+    await manageList<Job>('ignored-jobs', async (ignored) => {
         if (!ignored.some(item => item.id == jobId)) {
             const job = allJobs.find(j => j.id == jobId);
             job ? ignored.push(job) : console.log("Cannot find job");
@@ -109,7 +72,7 @@ export async function ignoreJob(jobId: string): Promise<void> {
 
 export async function viewJob(jobId: string): Promise<void> {
     console.log("View job " + jobId);
-    await withViewedJobs(async (viewed) => {
+    await manageList<string>('viewed', async (viewed) => {
         if (!viewed.includes(jobId)) {
             console.log("Mark job as viewed " + jobId);
             viewed.push(jobId);
@@ -119,7 +82,7 @@ export async function viewJob(jobId: string): Promise<void> {
 
 export async function unviewJob(jobId: string): Promise<void> {
     console.log("Unview job " + jobId);
-    await withViewedJobs(async (viewed) => {
+    await manageList<string>('viewed', async (viewed) => {
         if (viewed.includes(jobId)) {
             viewed.splice(viewed.indexOf(jobId), 1);
             console.log("Mark job as unviewed " + jobId);
@@ -129,7 +92,7 @@ export async function unviewJob(jobId: string): Promise<void> {
 
 export async function applyJob(jobId: string): Promise<void> {
     console.log("Apply job " + jobId);
-    await withAppliedJobs(async (applied) => {
+    await manageList<string>('applied', async (applied) => {
         if (!applied.includes(jobId)) {
             console.log("Mark job as applied " + jobId);
             applied.push(jobId);
@@ -139,7 +102,7 @@ export async function applyJob(jobId: string): Promise<void> {
 
 export async function unapplyJob(jobId: string): Promise<void> {
     console.log("Unapply job " + jobId);
-    await withAppliedJobs(async (applied) => {
+    await manageList<string>('applied', async (applied) => {
         if (applied.includes(jobId)) {
             applied.splice(applied.indexOf(jobId), 1);
             console.log("Mark job as unapplied " + jobId);
